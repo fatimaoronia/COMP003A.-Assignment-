9996ead002da4d979ae99831442136c12f599f98
fatimaoronia <115669547+fatimaoronia@users.noreply.github.com>
2024-01-21 23:35:22 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿// this is a a single-line comment
+/* this is a multi-line comment 
+ * 
+ * Author: Fatima Oronia
+ * Course: COMP-003A-L01
+ * Purpose: Code for Assignment 1
+ */
+
+
+namespace COMP003A.Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("100 Things About Me");
+            Console.WriteLine("1.My name is Fatima Oronia");
+            Console.WriteLine("2.I am 18 years old");
+            Console.WriteLine("3.My birthday is on December 11 2023");
+            Console.WriteLine("4.I have 2 siblings and I am the eldest");
+            Console.WriteLine("5.My mom named me after a religious figure");
+            Console.WriteLine("6.I have yet to obtain my drivers license");
+            Console.WriteLine("7.I am terrible at parking");
+            Console.WriteLine("8.I enjoy spending time in Mexico");
+            Console.WriteLine("9.I hope to have twins one day");
+            Console.WriteLine("10.I played soccer for 6 years");
+            Console.WriteLine("11.I enjoy spending time at the gym");
+            Console.WriteLine("12.I care a lot about health and wellness");
+            Console.WriteLine("13.I love to love");
+            Console.WriteLine("14.I have a cat named Chalino");
+            Console.WriteLine("15.I enjoy school most of the time");
+            Console.WriteLine("16.I have a best friend best friend named Steph");
+            Console.WriteLine("17.I have squish mellow called Fatima");
+            Console.WriteLine("18.My favorite artist is Frank Ocean");
+            Console.WriteLine("19.I used to be obsessed with pesto sandwich");
+            Console.WriteLine("20.I love cats so much");
+            Console.WriteLine("21.I hope to move to Oregon one day");
+            Console.WriteLine("22.Living next to the beach is one of my dreams");
+            Console.WriteLine("23.My goal for 2024 is t read more");
+            Console.WriteLine("24.I don't think anything is impossible");
+            Console.WriteLine("25.I am a very positive person most of the time");
+            Console.WriteLine("26.I hope to live in Mexico for a couple of years");
+            Console.WriteLine("27.Opening a bar in Mexico is one of my goals in life");
+            Console.WriteLine("28.I hope to open up a flower shop or bakery as an old lady");
+            Console.WriteLine("29.I love clothes");
+            Console.WriteLine("30.I have a bad spending habit");
+            Console.WriteLine("31.I love my parents");
+            Console.WriteLine("32.I plan on getting my masters and my phd");
+            Console.WriteLine("33.I have a really bad habit of daydreaming");
+            Console.WriteLine("34.I have to put on rainsounds to do my homework");
+            Console.WriteLine("35.I have programmed a magic 8 ball, a clock, and more");
+            Console.WriteLine("36.I am very thankful for my high school teacher for getting me into programming");
+            Console.WriteLine("37.I played softball in middle school");
+            Console.WriteLine("38.I did track in middle school and was very fast");
+            Console.WriteLine("39.I am supposed to be a senior in high school");
+            Console.WriteLine("40.If i ever did bad in a game I would remind myself that I was younger than everyone");
+            Console.WriteLine("41.I spend way too much time on pintrest");
+            Console.WriteLine("42.I regret not playing college soccer");
+            Console.WriteLine("43.My uncles took me everywhere with them when I was a little girl");
+            Console.WriteLine("44.I will never understand how people frown upon a friendship between opposite genders");
+            Console.WriteLine("45.I try to see the good in everyone");
+            Console.WriteLine("46.I started my first job at 17");
+            Console.WriteLine("47.I think I peaked at 14");
+            Console.WriteLine("48.My dream as a little girl was to become a singer");
+            Console.WriteLine("49.I have always thought about modeling");
+            Console.WriteLine("50.I could never be famous");
+            Console.WriteLine("51.I love when celebrities are mysterious");
+            Console.WriteLine("52.My favorite season is fall");
+            Console.WriteLine("53.The Twilight Series will always be my comfort movies");
+            Console.WriteLine("54.I hate insecure people");
+            Console.WriteLine("55.I have an obession with unsweetend tea");
+            Console.WriteLine("56.My current obsession is making orange creamsicles");
+            Console.WriteLine("57.I enjoy meeting and talking to new people");
+            Console.WriteLine("58.I find that it takes me a while to connect with others");
+            Console.WriteLine("59.I have attachment issues with others and my cat");
+            Console.WriteLine("60.I enjoy being alone");
+            Console.WriteLine("61.My favorite color is pink");
+            Console.WriteLine("62.I love the name cher");
+            Console.WriteLine("63.I used to spend a lot of time outside");
+            Console.WriteLine("64.I love france and hope to visit one day.");
+            Console.WriteLine("65.I have always wanted to visit Brazil");
+            Console.WriteLine("66.My celebrity crush is Jacob Elordi and Justin Bieber");
+            Console.WriteLine("67.I used to have Justin Bieber blanket");
+            Console.WriteLine("68.I can rap the fast part of the song rap god by eminem");
+            Console.WriteLine("69.I love making playlits");
+            Console.WriteLine("70.I find it crazy how we all have different music tastes");
+            Console.WriteLine("71.I believe that we all have different life times");
+            Console.WriteLine("72.I enhoy watching videos about history");
+            Console.WriteLine("73.I love making pintrest boards");
+            Console.WriteLine("74.Chipotle is my favorite fast food");
+            Console.WriteLine("75.I am convinced that I could live off creamsicle icecream");
+            Console.WriteLine("76.I reallu do not like icecream very much");
+            Console.WriteLine("77.I will never understand bluetooth");
+            Console.WriteLine("78.I hope to create an app and for it to become pipular worldwide");
+            Console.WriteLine("79.I used to be obsessed with the idea of becoming a makeup artist");
+            Console.WriteLine("80.I think museums are absolutely amazing");
+            Console.WriteLine("81.I could see myself becoming an artist");
+            Console.WriteLine("82.I like to argue");
+            Console.WriteLine("83.I love the idea of becoming a lawyer");
+            Console.WriteLine("84.The movie Legally Blonde almost convinced me to pursue the idea of becoming a lawyer");
+            Console.WriteLine("85.My favorite fruit is pomogranite");
+            Console.WriteLine("86.I will eat something everyday until I get tired of it");
+            Console.WriteLine("87.Doc Martens and New Balance are my favorite shoes");
+            Console.WriteLine("88.My dream is to get married by the beach");
+            Console.WriteLine("89.I will not get married until I can afford my dream wedding");
+            Console.WriteLine("90.I love makeup");
+            Console.WriteLine("91.I love blowouts");
+            Console.WriteLine("92.I always want to change my hait");
+            Console.WriteLine("93.I have cried about my hair changing multiple times");
+            Console.WriteLine("94.Spicy Buldak noodles are my favorite thing ever");
+            Console.WriteLine("95.I like the feeling of being tired after running");
+            Console.WriteLine("96.I like the broncos");
+            Console.WriteLine("97.I love taking pictures in my digital camera");
+            Console.WriteLine("98.It takes me a while to get used to something");
+            Console.WriteLine("99.I absolutely love matcha");
+            Console.WriteLine("100.I love the feelings of finally debugging something");
+        }
+    }
+}
+
+
